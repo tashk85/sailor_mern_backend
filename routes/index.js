a9f63a7ee9cc463ca142e4ccdfa676753a9b72f9
Natasha <tashk85@gmail.com>
2019-07-11 09:37:52 +1000

    added auth_routes link to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const AuthRoutes = require("./auth_routes");
 
 router.get("/", (req, res) => res.send("Welcome"));
+router.use("/auth", AuthRoutes);
 
 module.exports = router;
