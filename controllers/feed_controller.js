887611b54ea4e73439fea00451f0b3d57b6bdccb
Alex <atdiwa@gmail.com>
2019-07-16 23:18:10 +1000

    fetch multiple RSS feeds for display on front end

--- controllers/feed_controller.js
+++ controllers/feed_controller.js
@@ -1,17 +1,17 @@
 const ArticleModel = require("./../database/models/article_model");
-const  RssFetch = require("./../services/rss_service");
+const rssService = require("./../services/rss_service");
 
-function index(req,res){
-    // const email = req.user.email;
-    RssFetch.RssMedCity();
-    // RssFetch.Trial();
-    // RssFetch.getIndividualArticle();
-    return res.json("feed testing!")
-    // return res.render("feed/feed", { email });
+const feeds = [
+    "https://medcitynews.com/feed/", 
+    "https://www.digitalhx.com/feed/", 
+    "https://www.healthcareitnews.com/home/feed"
+]
+
+async function index(req,res){
+    feeds.forEach(feed => rssService.fetchRSS(feed));
+    const articles = await ArticleModel.find().sort({"date_posted": -1});;
+    return res.send({ articles });
 }
-
-
-
 
 
 module.exports = {
