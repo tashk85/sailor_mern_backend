4141c79104a688180c7f413f0d634dd700576450
Alex <atdiwa@gmail.com>
2019-07-17 16:29:30 +1000

    add CRON job and method for saving article to db by url

--- app.js
+++ app.js
@@ -46,4 +46,6 @@ app.use(express.static("public"));
 
 app.use(require("./middleware/error_middleware"));
 
+require("./services/rss_cron");
+
 module.exports = app;
--- controllers/feed_controller.js
+++ controllers/feed_controller.js
@@ -1,15 +1,9 @@
 const ArticleModel = require("./../database/models/article_model");
-const rssService = require("./../services/rss_service");
-
-const feeds = [
-    "https://medcitynews.com/feed/", 
-    "https://www.digitalhx.com/feed/", 
-    "https://www.healthcareitnews.com/home/feed"
-]
+const RSSService = require("./../services/rss_service");
 
 async function index(req,res){
-    feeds.forEach(feed => rssService.fetchRSS(feed));
-    const articles = await ArticleModel.find().sort({"date_posted": -1});;
+    RSSService.addArticle("https://medcitynews.com/2019/07/ai-drug-discovery-company-recursion-pharmaceuticals-raises-121m-series-c/");
+    const articles = await ArticleModel.find().sort({"date_posted": -1});
     return res.send({ articles });
 }
 
