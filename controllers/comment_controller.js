7a94bdd1350f6a025277c6bb6133ca700a1da018
leahhou <hsx_124@hotmail.com>
2019-07-22 16:16:40 +1000

    fix bug: sending only user's name & id to the frontend

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -10,7 +10,19 @@ async function index(req, res, next) {
     
     // send back all users' first&last name&userId for mention functions
     let users = await UserModel.find({});
-    return res.send({ article, users});
+    let filteredUsers = [];
+    users.forEach((user)=>{
+        let { firstName, lastName, _id:userId } = user;
+        filterdUser = {firstName, lastName, userId};
+        console.log(`is each user filtered? ${filterdUser}`);
+        filteredUsers.push(filterdUser);
+        console.log(`is ALL users filtered? ${filteredUsers} INSIDE loop`);
+        return filteredUsers;
+    })
+
+    console.log(`is ALL users filtered? ${filteredUsers} OUTSIDE loop`);
+
+    return res.send({ article, filteredUsers});
 }
 
 // API to create comment
