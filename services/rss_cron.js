95bfa8d2a833d098e911b7be1e92f1c97352a323
leahhou <hsx_124@hotmail.com>
2019-07-23 16:31:04 +1000

    cron job fix

--- services/rss_cron.js
+++ services/rss_cron.js
@@ -8,14 +8,14 @@ const feeds = [
 ]
 
 // Intial fetch upon starting up server
-// feeds.forEach(feed => rssService.fetchRSS(feed));
+feeds.forEach(feed => rssService.fetchRSS(feed));
 
-// // */10 -> triggers every 2 hours
-// // Midnight: "00 00 00 * * *";
-// const job = new CronJob('* * */2 * * *', function() {
-//   feeds.forEach(feed => rssService.fetchRSS(feed));
-//   const d = new Date();
-//   console.log("Fetched RSS feed", d);
-// })
+// */10 -> triggers every 2 hours
+// Midnight: "00 00 00 * * *";
+const job = new CronJob('* * */2 * * *', function() {
+  feeds.forEach(feed => rssService.fetchRSS(feed));
+  const d = new Date();
+  console.log("Fetched RSS feed", d);
+})
 
-// job.start();
+job.start();
