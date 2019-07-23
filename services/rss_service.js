ed7b6a540b02e9d5376678275fe49649c98ed0e3
Natasha <tashk85@gmail.com>
2019-07-23 10:40:09 +1000

    Removed interests tags from seed file, now rss_service adds interests from random interests function

--- services/rss_service.js
+++ services/rss_service.js
@@ -1,14 +1,31 @@
 const Parser = require("rss-parser");
 const parser = new Parser();
 const ArticleModel = require("./../database/models/article_model");
+const InterestModel = require("./../database/models/interest_model");
 const { extract, extractWithEmbedly } = require("article-parser");
+
+// Method that generates random interests to push into article interests
+function generateRandomInterests() {
+    const interestTags = InterestModel.schema.path('tag').enumValues;
+    let numOfInterests = Math.floor(Math.random() * 3 + 1);
+    let randomInterests = [];
+
+    for(let i = 0; i <= numOfInterests; i++) {
+        let interest = interestTags[Math.floor(Math.random()*interestTags.length)];
+        if (!randomInterests.includes(interest)) {
+            randomInterests.push(interest);
+        }
+    }
+
+    return randomInterests;
+}
 
 // Method that fetches article information from RSS feeds and saves data to database
 async function fetchRSS(url) {
     let feed = await parser.parseURL(url);
     feed.items.forEach(async item => {        
         const articleURL = item.link;
-
+        console.log(generateRandomInterests());
         // We need to extract information slightly differently for the DHX feed due to its formatting
         const dhx = "https://www.digitalhx.com/feed/";
         let isDHX = false;
@@ -26,10 +43,9 @@ async function fetchRSS(url) {
                     source: feed.title,
                     url: item.link,
                     image: isDHX ? item.enclosure.url : article.image,
-                    // rssCategories: item.categories,
-                    // localCategories: importCategories
                 },
-                article_body: isDHX ? item['content:encoded'] : article.content
+                article_body: isDHX ? item['content:encoded'] : article.content,
+                interests: generateRandomInterests()
             })
         } catch(error) {
             console.log("***************************  Ignore if E11000: article has already been saved to database  ********************************");
@@ -37,6 +53,7 @@ async function fetchRSS(url) {
         }
         isDHX = false;
     })
+    
     return console.log("All articles saved to database");
 };
 
